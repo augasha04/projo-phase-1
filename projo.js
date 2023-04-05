77072c6ebb76cf71331e541d4726d42bbf9aa900
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-05 15:25:56 +0300

    doing js

--- projo.js
+++ projo.js
@@ -1,4 +1,11 @@
-fetch("")
+fetch("http://localhost:3000/drinks")
+  .then(response => response.json())
+  .then(data => {
+    // use the fetched data here
+    console.log(data);
+  })
+  .catch(error => {
+    // handle any errors here
+    console.error(error);
+  });
 
-
-// create
