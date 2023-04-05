476fbacedbf838d2e44f32777928e1a898886ef1
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-05 16:28:39 +0300

    doing js

---
+++ projo.js
@@ -0,0 +1,10 @@
+const orderDrinkBtn = document.getElementById('order-drink-btn');
+    const orderDrinkForm = document.getElementById('order-drink-form');
+  
+    orderDrinkBtn.addEventListener('click', () => {
+      if (orderDrinkForm.style.display === 'none') {
+        orderDrinkForm.style.display = 'block';
+      } else {
+        orderDrinkForm.style.display = 'none';
+      }
+    });
