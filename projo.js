010691483a26ee05aaa4d2fe074c81b25f8dd99d
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-05 16:07:13 +0300

    doing js

--- projo.js
+++ projo.js
@@ -19,8 +19,22 @@ fetch("http://localhost:3000/drinks")
 const buyButton = card.querySelector('order-drink');
 buyButton.addEventListener('click', function(event) {
   const availableDrinks = card.querySelector('available-drinks');
-  const numAvailableTickets = parseInt(availableDrinks.textContent);
+  const numAvailableDrinks = parseInt(availableDrinks.textContent);
 })
+
+
+//the code down here updates drinks
+availableDrinks.textContent = availableDrinks;
+const newDrinksSold = movie.strDrink;
+fetch(`http://localhost:3000/drinks${drinks.id}`, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ strDrink: newDrinksSold })
+})
+
+alert('Drink already Sold Out');
 
 
 
