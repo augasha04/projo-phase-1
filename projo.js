c5f9ff4d25a8ce3b3def9d9e6db68b86efe6467a
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-04-06 08:49:37 +0300

    final touches

--- projo.js
+++ projo.js
@@ -5,6 +5,19 @@ const orderDrinkBtn = document.getElementById('order-drink-btn');
       if (orderDrinkForm.style.display === 'none') {
         orderDrinkForm.style.display = 'block';
       } else {
-        orderDrinkForm.style.display = 'block';
+        orderDrinkForm.style.display = 'none';
       }
     });
+
+
+    // fetching data
+    fetch("")
+  .then(response => response.json())
+  .then(data => {
+    // use the fetched data here
+    console.log(data);
+  })
+  .catch(error => {
+    // handle any errors here
+    console.error(error);
+  });
